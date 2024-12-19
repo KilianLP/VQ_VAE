13d955337585f21729c8f8f31b510de3b79c359c
KilianLP <157063461+KilianLP@users.noreply.github.com>
2024-12-19 09:39:06 +0100

    Create visualize_reconstruction.py

---
+++ visualize_reconstruction.py
@@ -0,0 +1,40 @@
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+images = []
+for i in range(5):
+    k = random.randrange(1, len(test_dataset))
+    image = test_dataset.__getitem__(k)
+    image = image.permute(1,2,0)
+    image = (image + 1) / 2 * (data_max - data_min) + data_min
+
+    image = image.numpy()
+    images.append(image)
+
+    autoencoder.eval()
+    image = test_dataset.__getitem__(k)
+    image, _= autoencoder(image.unsqueeze(0).to(device))
+    image = image.squeeze(0)
+    image = image.cpu()
+    image = image.permute(1,2,0)
+    image = (image + 1) / 2 * (data_max - data_min) + data_min
+    image = image.detach().numpy()
+
+    images.append(image)
+
+
+fig, axs = plt.subplots(5,2 , figsize=(6, 15))
+
+
+for i, ax in enumerate(axs.flat):
+    ax.imshow(images[i], aspect='auto', cmap='inferno', origin='lower')
+    if i % 2 == 0:
+        ax.set_title(f'Original Image {i//2+1}')
+    else:
+      ax.set_title(f'Reconstructed Image {i//2 + 1}')
+    ax.axis('off')
+
+plt.tight_layout()
+plt.show()
